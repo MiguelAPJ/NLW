609593df7c9cccfbd3dcb7964f0985f87322b496
miguel.junior <miguelmialpiju@gmail.com>
2023-05-16 10:10:31 -0300

    Atualização do app.tsx no mobile

--- nlw12-ignite/mobile/App.tsx
+++ nlw12-ignite/mobile/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="bg-zinc-950 flex-1 items-center justify-center">
       <Text className="text-zinc-50">OI</Text>
-      <StatusBar style="light" translucent />
+      <StatusBar style="light" translucent  />
     </View>
   );
 }
