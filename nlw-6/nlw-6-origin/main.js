b88eee213deb47b5db125b6ad65d0b0c2ee0c16f
miguel.junior <miguelmialpiju@gmail.com>
2024-01-11 16:32:08 -0300

    pagina e footer pronto

--- nlw-6/nlw-6-origin/main.js
+++ nlw-6/nlw-6-origin/main.js
@@ -27,3 +27,36 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+// Testimonials carousel slider
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// ScrollReveal: mostrar elementos quando der scroll na pagina
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  
+  
+  
+  `,
+  { interval: 100 }
+)
