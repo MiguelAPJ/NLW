671e30436b14d181034122bdbdea54c911279c58
miguel.junior <miguelmialpiju@gmail.com>
2024-02-19 15:38:11 -0300

    Segunda aula finalizada, cards prontos, interface para adicionar nota pronto, ficou faltando salvar a nota e adicionar texto por voz.

---
+++ nlw-expert/nlw-expert-react-notes/extensoes.md
@@ -0,0 +1,3 @@
+Biblioteca
+
+Sonner = Ela server para enviar um alerta na tela, para mostrar se deu certo ou errado
--- nlw-expert/nlw-expert-react-notes/src/app.tsx
+++ nlw-expert/nlw-expert-react-notes/src/app.tsx
@@ -2,6 +2,8 @@ import logo from './assets/Logo.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+
+
 export function App() {
  return (
    <div className="mx-auto max-w-6xl my-12 space-y-6 ">
@@ -21,11 +23,13 @@ export function App() {
       <div className="grid grid-cols-3 gap-4 auto-rows-[250px]">
         
          <NewNoteCard />
-         <NoteCard />
-         <NoteCard /> 
-         <NoteCard /> 
+         <NoteCard note={{
+            date: new Date(),
+            content: 'oi'
+         }} />
+         
                      
-
+ 
       </div>
 
    </div>
